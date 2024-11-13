daa394185be695de6b5a93cde5e156f1ed59c777
gy1022 <bokowife2@naver.com>
2024-11-13 09:56:51 +0900

    [Feat] 간단한 아이템 수집 및 인벤토리 제작

---
+++ Assets/Script/ItemDetector.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum ItemType
+{
+    Crystal,
+    Plant,
+    Bush,
+    Tree,
+
+}
+public class ItemDetector : MonoBehaviour
+{
+    public float chekRadius = 3.0f;
+    private Vector3 lastPosition;
+    private float moveThreshold = 0.1f;
+    private CollectibleItem currentNearbyItem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPosition = transform.position;
+        CheckForItem();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(lastPosition, transform.position) > moveThreshold)
+        {
+            CheckForItem();
+            lastPosition = transform.position;
+        }
+
+        if (currentNearbyItem != null && Input.GetKeyDown(KeyCode.E))
+        {
+            currentNearbyItem.CollectItem(GetComponent<PlayerInventory>());
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, chekRadius);
+    }
+
+    private void CheckForItem()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, chekRadius);
+        
+        float closestDistance = float.MaxValue;
+        CollectibleItem closestItem = null;
+
+        foreach (Collider collider in hitColliders)
+        {
+            CollectibleItem item = collider.GetComponent<CollectibleItem>();
+            if (item != null && item.canCollect)
+            {
+                float distance = Vector3.Distance(transform.position, item.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestItem = item;
+                }
+            }
+        }
+        if (closestItem != currentNearbyItem)
+        {
+            currentNearbyItem = closestItem;
+            if (currentNearbyItem != null)
+            {
+                Debug.Log($"[E] 키를 눌러 {currentNearbyItem.itemName} 수집");
+            }
+        }
+    }
+
+}
